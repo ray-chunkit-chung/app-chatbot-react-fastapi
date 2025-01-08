bb3572bcc0f158369913032e7bc3af945837eeba
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2025-01-08 20:58:57 +0900

    try frontend code

--- frontend/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// src/components/index.ts
-
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as Button } from './Button';
-// Add more component exports as needed
