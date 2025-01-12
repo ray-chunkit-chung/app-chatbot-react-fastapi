bfca13df56c1b666d21a007278f908f098c8e37f
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2025-01-12 11:21:14 +0900

    set up page and layout

---
+++ frontend/app/components/chat-section.tsx
@@ -0,0 +1,23 @@
+export default function ChatSection() {
+    // const { backend } = useClientConfig();
+    // const handler = useChat({
+    //   api: `${backend}/api/chat`,
+    //   onError: (error: unknown) => {
+    //     if (!(error instanceof Error)) throw error;
+    //     let errorMessage: string;
+    //     try {
+    //       errorMessage = JSON.parse(error.message).detail;
+    //     } catch (e) {
+    //       errorMessage = error.message;
+    //     }
+    //     alert(errorMessage);
+    //   },
+    // });
+    return (
+        <p>Hello</p>
+    //   <ChatSectionUI handler={handler} className="w-full h-full">
+    //     <CustomChatMessages />
+    //     <CustomChatInput />
+    //   </ChatSectionUI>
+    );
+  }
